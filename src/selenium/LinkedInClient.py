ed89c9bc7ed26715e034a23eefad1c7231b58fe8
Ilya Radinsky <ilya_radinsky@hotmail.com>
2023-01-31 04:08:52 +0200

    Implemented getting the connection list

--- src/selenium/LinkedInClient.py
+++ src/selenium/LinkedInClient.py
@@ -1,8 +1,13 @@
-
 import getpass
 import logging
+from random import randint
 from selenium import webdriver
+from selenium.webdriver import ActionChains
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+import time
 from typing import Any, Dict
 
 
@@ -12,7 +17,7 @@ default_capabilities = {
         "enableVNC": False,
         "enableVideo": False,
         "screenResolution": "640x480",
-    }
+    },
 }
 
 
@@ -25,28 +30,103 @@ class SeleniumClient:
         self.driver.quit()
 
     def login(self, use_verification_pin=True):
+        self.driver.get("https://www.linkedin.com/")
+        self.driver.set_window_size(1428, 755)
+
         email = input("Email: ")
         password = getpass.getpass("Password: ")
 
-        self.driver.get("https://www.linkedin.com/")
-        self.driver.set_window_size(1428, 755)
         self.driver.find_element(By.ID, "session_key").click()
         self.driver.find_element(By.ID, "session_key").send_keys(email)
         self.driver.find_element(By.ID, "session_password").click()
         self.driver.find_element(By.ID, "session_password").click()
         self.driver.find_element(By.ID, "session_password").send_keys(password)
-        self.driver.find_element(By.CSS_SELECTOR, ".sign-in-form__submit-button").click()
+        self.driver.find_element(
+            By.CSS_SELECTOR, ".sign-in-form__submit-button"
+        ).click()
 
         if use_verification_pin:
             pin = input("Pin: ")
-            self.driver.find_element(By.ID, "input__phone_verification_pin").send_keys(pin)
+            self.driver.find_element(By.ID, "input__phone_verification_pin").send_keys(
+                pin
+            )
             self.driver.find_element(By.ID, "two-step-submit-button").click()
 
-        self.driver.get("https://www.linkedin.com/mynetwork/")
+    def simulate_pause(self, start=5, end=8):
+        time.sleep(randint(start, end))
+
+    def simulate_press_end(self):
+        ActionChains(self.driver).send_keys(Keys.END).perform()
+
+    def try_to_click_more_results(self):
+        ActionChains(self.driver).send_keys(Keys.HOME).perform()
+        time.sleep(0.5)
+
+    def get_my_connections(self):
+        self.go_to_my_connections()
+
+        total_connections = self.get_total_connections()
+        logging.info(f"Total connections: {total_connections}")
+
+        retries = 0
+        prev_connections = []
+        found_connections = self.get_connections()
+
+        while len(found_connections) < total_connections:
+            self.try_to_click_more_results()
+            self.simulate_press_end()
+            self.simulate_pause(1, 3)
+            prev_connections = found_connections
+            found_connections = self.get_connections()
+
+            if len(found_connections) == len(prev_connections):
+                retries = retries + 1
+
+                if retries == 3:
+                    break
+            else:
+                retries = 0
+
+        logging.info(f"Found connections: {len(found_connections)}")
+
+        return found_connections
+
+    def go_to_my_connections(self):
+        self.driver.get(
+            "https://www.linkedin.com/mynetwork/invite-connect/connections/"
+        )
+        self.simulate_pause(1, 5)
+
+    def get_total_connections(self):
+        text = self.driver.find_element(By.CSS_SELECTOR, "header h1").text.strip()
+        return int(text.split(" ")[0].replace(",", ""))
+
+    def get_connections(self):
+        profiles = []
+        try:
+            connections = self.driver.find_elements(By.CLASS_NAME, "mn-connection-card")
+            for conn in connections:
+                anchor = conn.find_element(By.CLASS_NAME, "mn-connection-card__picture")
+                profile_link = anchor.get_attribute("href")
+                occupation = conn.find_element(
+                    By.CLASS_NAME, "mn-connection-card__occupation"
+                )
+                profile_occupation = occupation.get_attribute("innerHTML").strip()
+                name = conn.find_element(By.CLASS_NAME, "mn-connection-card__name")
+                profile_name = name.get_attribute("innerHTML").strip()
+                profiles.append(
+                    {
+                        "profile_occupation": profile_occupation,
+                        "profile_link": profile_link,
+                        "profile_name": profile_name,
+                    }
+                )
+        except:
+            logging.error("Getting connections")
+        return profiles
 
 
-
-def main(log_level='DEBUG'):
+def main(log_level="INFO"):
     logging.basicConfig(
         level=logging.getLevelName(log_level),
         format="%(asctime)s %(levelname)-5s %(name)s (%(module)s-%(lineno)s): %(message)s",
@@ -54,7 +134,8 @@ def main(log_level='DEBUG'):
 
     client = SeleniumClient()
     client.login()
+    client.get_my_connections()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
