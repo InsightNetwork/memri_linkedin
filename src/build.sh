73df7cfb355b9956d2c350b7cb181dd283cc3a20
Ilya Radinsky <ilya_radinsky@hotmail.com>
2023-02-13 09:50:33 +0200

    Updated the build script

--- src/build.sh
+++ src/build.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-docker build --tag memri_linkedin/ui:1.0 .
+docker build --tag memri_linkedin/ui:1.0 --build-arg VIS_OWNER_KEY=$VIS_OWNER_KEY --build-arg VIS_DATABASE_KEY=$VIS_DATABASE_KEY .
